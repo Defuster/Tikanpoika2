886bdfc73ccdc06080cffe91ea0414122f725f06
a1503078 <a1503078@ODWIN10PROD044>
2018-09-13 14:11:25 +0300

    Add project files.

---
+++ WebApplication5/Controllers/TuotteetController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class TuotteetController : Controller
+    {
+        private TikanpoikaEntities db = new TikanpoikaEntities();
+
+        // GET: Tuotteet
+        public ActionResult Index()
+        {
+            return View(db.Tuote.ToList());
+        }
+
+        // GET: Tuotteet/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tuote tuote = db.Tuote.Find(id);
+            if (tuote == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tuote);
+        }
+
+        // GET: Tuotteet/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tuotteet/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TuoteID,Nimi,Hinta,Veroprosentti")] Tuote tuote)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tuote.Add(tuote);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tuote);
+        }
+
+        // GET: Tuotteet/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tuote tuote = db.Tuote.Find(id);
+            if (tuote == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tuote);
+        }
+
+        // POST: Tuotteet/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TuoteID,Nimi,Hinta,Veroprosentti")] Tuote tuote)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tuote).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tuote);
+        }
+
+        // GET: Tuotteet/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tuote tuote = db.Tuote.Find(id);
+            if (tuote == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tuote);
+        }
+
+        // POST: Tuotteet/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tuote tuote = db.Tuote.Find(id);
+            db.Tuote.Remove(tuote);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
